a4efd39d92634d3172f576140e6d197d871a71e5
Lena Hartmann <lena.hartmann00@googlemail.com>
2019-09-26 23:27:22 +0200

    fixed domains

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(www.lena-hartmann.com)
+domains=(lena-hartmann.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="lena.hartmann@protonmail.com" # Adding a valid address is strongly recommended
